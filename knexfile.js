11868edacbd6e1fb59bf876ef59b8eb176961294
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-19 13:58:07 -0400

    :sparkles: adds postgres
    adds postgres

--- knexfile.js
+++ knexfile.js
@@ -18,16 +18,10 @@ module.exports = {
     }
   },
   production: {
-    client: "sqlite3",
+    client: "pg",
     useNullAsDefault: true,
-    connection: {
-      filename: "./data/shouts.db3"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    },
+    connection: process.env.DATABASE_URL,
+
     migrations: {
       directory: "./data/migrations"
     },
